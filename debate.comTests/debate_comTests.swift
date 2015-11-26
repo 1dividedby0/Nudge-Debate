a59f21d10c836492abf7c5106271157bd66b6b97
Dhruv Mangtani <turbodhruv@gmail.com>
2015-11-26 09:51:57 -0800

    Initial Commit

---
+++ debate.comTests/debate_comTests.swift
@@ -0,0 +1,36 @@
+//
+//  debate_comTests.swift
+//  debate.comTests
+//
+//  Created by Dhruv Mangtani on 11/26/15.
+//  Copyright © 2015 dhruv.mangtani. All rights reserved.
+//
+
+import XCTest
+@testable import debate_com
+
+class debate_comTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
